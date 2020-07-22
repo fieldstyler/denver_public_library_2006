ff0e1d5d83969d5370373b06b25d11efa0cf4f3a
fieldstyler <tdfields93@gmail.com>
2020-07-22 14:17:44 -0600

    Add test_author_publication_time_frame, unfinished

--- test/library_test.rb
+++ test/library_test.rb
@@ -30,5 +30,19 @@ class LibraryTest < Minitest::Test
   dpl.add_author(harper_lee)
   assert_equal [charlotte_bronte, harper_lee], dpl.authors
   end
-  
+
+  def test_author_publication_time_frame
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor   = charlotte_bronte.write("The Professor", "1857")
+    villette    = charlotte_bronte.write("Villette", "1853")
+    harper_lee  = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    assert_equal ({:start=>"1847", :end=>"1857"}), dpl.publication_time_frame_for(charlotte_bronte)
+    assert_equal ({:start => "1960", :end => "1960"}), dpl.publication_time_frame_for(harper_lee)
+  end
+
 end
