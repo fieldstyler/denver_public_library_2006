c92a3a0e3a03589713f9ae0f3b6d3cd47fad57bf
fieldstyler <tdfields93@gmail.com>
2020-07-22 13:49:17 -0600

    Update test_can_write_a_book. Can't add books to @books array, though.

--- test/author_test.rb
+++ test/author_test.rb
@@ -17,7 +17,14 @@ class AuthorTest < Minitest::Test
   end
 
   def test_author_can_write_a_book
-
-  end 
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    assert_equal Book, jane_eyre.class
+    assert_equal "Jane Eyre", jane_eyre.title
+    villette = charlotte_bronte.write("Villette", "1853")
+    require "pry"; binding.pry
+    assert_equal "Villette", villette.title
+    assert_equal [jane_eyre, villette], charlotte_bronte.books
+  end
 
 end
