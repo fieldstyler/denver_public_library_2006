04a3bc64d9b3c99fb9d741e7d928ce148658953c
fieldstyler <tdfields93@gmail.com>
2020-07-22 12:52:07 -0600

    Create initialize method with #name and #books as readable attributes

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,10 @@
 class Author
 
-end 
+  attr_reader :name,
+              :books
+  def initialize(author_info)
+    @name   = "#{author_info[:first_name]} #{author_info[:last_name]}"
+    @books  = []
+  end
+
+end
